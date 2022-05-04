1012665964d1fa36c0c1e50d9bc74b7ba56687c9
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-04 17:43:45 -0500

    adding section and styled

--- src/App.js
+++ src/App.js
@@ -8,10 +8,21 @@ const Container = styled.div`
   height: 100%;
   align-items: center;
 `
+
+const Section = styled.section`
+  background-color : #eee;
+  border-top: solid 2px palevioletred;
+  padding : 20px 25px;
+  width: 500px;
+  box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
+`
+
 const App= () => {
   return (
     <Container>
-      lala
+      <Section>
+        kkaka
+      </Section>
     </Container>
   );
 }
