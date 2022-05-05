bfd9719130c4c05668b135fdce43fb376bc7b300
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-05 15:44:27 -0500

    adding the logic and formula of compound interest

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import Input from './components/Input'
 import Section from './components/Section'
 
 
+const compoundInterest = (deposit, contribution, years, rate) =>{
+  let total = deposit
+  for (let i =0; i< years;i++){
+    total = (total+contribution) * (rate + 1)
 
+  }
+  return Math.round(total)
+}
 const App= () => {
-  const handleSubmit = () => {}
+  const handleSubmit = ({deposit,contribution,years,rate}) => {
+    const val = compoundInterest(Number(deposit),Number (contribution),Number (years),Number(rate))
+    console.log(val)
+  }
   return (
     <Container>
       <Section>
